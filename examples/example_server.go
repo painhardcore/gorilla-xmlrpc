0734c4e4381efa61aca451e9ad4e8e8212f48836
Andrey Yurchenkov <painhardcore@gmail.com>
2020-05-04 12:32:30 +0300

    change examples

--- examples/example_server.go
+++ examples/example_server.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/rpc"
-    "github.com/divan/gorilla-xmlrpc/xml"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/rpc"
+	"github.com/painhardcore/gorilla-xmlrpc/xml"
 )
 
 type HelloService struct{}
 
-func (h *HelloService) Say(r *http.Request, args *struct{Who string}, reply *struct{Message string}) error {
-    log.Println("Say", args.Who)
-    reply.Message = "Hello, " + args.Who + "!"
-    return nil
+func (h *HelloService) Say(r *http.Request, args *struct{ Who string }, reply *struct{ Message string }) error {
+	log.Println("Say", args.Who)
+	reply.Message = "Hello, " + args.Who + "!"
+	return nil
 }
 
 func main() {
-    RPC := rpc.NewServer()
-    xmlrpcCodec := xml.NewCodec()
-    RPC.RegisterCodec(xmlrpcCodec, "text/xml")
-    RPC.RegisterService(new(HelloService), "")
-    http.Handle("/RPC2", RPC)
+	RPC := rpc.NewServer()
+	xmlrpcCodec := xml.NewCodec()
+	RPC.RegisterCodec(xmlrpcCodec, "text/xml")
+	RPC.RegisterService(new(HelloService), "")
+	http.Handle("/RPC2", RPC)
 
-    log.Println("Starting XML-RPC server on localhost:1234/RPC2")
-    log.Fatal(http.ListenAndServe(":1234", nil))
+	log.Println("Starting XML-RPC server on localhost:1234/RPC2")
+	log.Fatal(http.ListenAndServe(":1234", nil))
 }
